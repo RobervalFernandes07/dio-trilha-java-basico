835862747c979f1bb820bb8ac464e5b4720bf616
RobervalFernandes07 <robervalfernandesdigital@outlook.com>
2024-08-15 11:28:06 -0300

    Novo-Projeto

--- métodos/metodos/src/metodos.java
+++ métodos/metodos/src/metodos.java
@@ -1,5 +1,5 @@
-public class TiposVariaveis {
+public class metodos {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        System.out.println("Hello, World!");
     }
 }
