edd1444cf94249283b50d2cdfdfee3d73645435f
RobervalFernandes07 <robervalfernandesdigital@outlook.com>
2024-08-21 15:53:16 -0300

    Exemplos de for/arrays

---
+++ Controle-de-fluxo/src/ExemploForArray.java
@@ -0,0 +1,16 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+
+
+        String alunos[] = {"Felipe","Jonas", "Julia", "Marcos", "Roberval","Fernandes"};
+
+        // Forma simplificada
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é : " + aluno);
+        }
+        /*for(int x=0; x < alunos.length; x ++){
+            System.out.println("O aluno no indece x = " + x + " é " + alunos[x]);
+        }*/
+    }
+}
